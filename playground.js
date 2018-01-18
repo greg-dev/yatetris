9ac963f5565ee642f596998bd58f9b5a8f049aca
Greg Dev <greg.bak.dev@gmail.com>
2018-01-19 00:46:27 +0100

    Add second game instance on playground page for spawn locations testing

--- playground.js
+++ playground.js
@@ -5,9 +5,18 @@
 window.onload = function () {
   var game1 = new Game({
     rows: 10,
-    cells: 6,
+    cells: 8,
     view: new ViewCanvas({
       root: document.getElementById('game1')
+    }),
+    input: new InputKeyboard()
+  });
+
+  var game2 = new Game({
+    rows: 10,
+    cells: 5,
+    view: new ViewCanvas({
+      root: document.getElementById('game2')
     }),
     input: new InputKeyboard()
   });
