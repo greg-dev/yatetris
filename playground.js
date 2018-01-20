33aab745fe53c4c1963c4e1d5cae896404080b6a
Greg Dev <greg.bak.dev@gmail.com>
2018-01-20 23:57:44 +0100

    Add example game with custom tetrominos set on playground page

--- playground.js
+++ playground.js
@@ -20,4 +20,58 @@ window.onload = function () {
     }),
     input: new InputKeyboard()
   });
+
+  var game3 = new Game({
+    rows: 20,
+    cells: 6,
+    view: new ViewCanvas({
+      root: document.getElementById('game3')
+    }),
+    input: new InputKeyboard(),
+    tetrominos: {
+      small: {
+        shape: [
+          [
+            [1]
+          ]
+        ],
+        line: 1
+      },
+      big: {
+        shape: [
+          [
+            [1, 1, 1],
+            [1, 1, 1],
+            [1, 1, 1]
+          ]
+        ],
+        line: 0
+      },
+      fck: {
+        shape: [
+          [
+            [0, 1, 0],
+            [0, 1, 0],
+            [1, 1, 1]
+          ],
+          [
+            [1, 0, 0],
+            [1, 1, 1],
+            [1, 0, 0]
+          ],
+          [
+            [1, 1, 1],
+            [0, 1, 0],
+            [0, 1, 0]
+          ],
+          [
+            [0, 0, 1],
+            [1, 1, 1],
+            [0, 0, 1]
+          ]
+        ],
+        line: 0
+      }
+    }
+  });
 };
