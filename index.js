8db8d7428a4ebe71d1c24df12a8243b411428c28
Greg Dev <greg.bak.dev@gmail.com>
2018-01-24 01:51:52 +0100

    Set up home page with the most simple example

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
 'use strict';
 
-/* eslint-disable no-unused-vars */
-
 window.onload = function () {
-  var game1 = new Game({
-    view: new ViewCanvas({
-      root: document.getElementById('game1')
-    }),
-    input: new InputKeyboard()
+  new Game({
+    id: 'default'
   });
 };
